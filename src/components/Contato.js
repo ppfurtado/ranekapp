d6e9b92d3357d6edc0cb1fef3b5d122259dc5a8a
Pedro Furtado <furtado.pedropaulo@gmail.com>
2020-10-06 23:56:42 -0300

    add contato

--- src/components/Contato.js
+++ src/components/Contato.js
@@ -1,11 +1,20 @@
 import React from 'react'
 
 import styles from './Contato.module.css'
+import foto from '../img/contato.jpg'
 
 const Contato = () => {
   return (
     <section className={styles.contato}>
-      <h1>Contato</h1>
+      <img src={foto} alt="Máquina descrever"/>
+      <div>
+        <h1>Entre em contato</h1>
+        <ul className={styles.dados}>
+          <li>furtado.pedropaulo@gmail.com</li>
+          <li>99999-9999</li>
+          <li>Carinha que mora logo ali</li>
+        </ul>
+      </div>
     </section>
   )
 }
