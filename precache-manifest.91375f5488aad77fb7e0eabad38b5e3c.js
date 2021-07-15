d6b4e6c576fdf0435296f9a5ffb5e4c698e2f688
ppfurtado <furtado.pedropaulo@gmail.com>
2021-07-15 01:17:33 -0300

    Updates

---
+++ precache-manifest.91375f5488aad77fb7e0eabad38b5e3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9759b7cd7199ea72de8d7b7deb28c07",
+    "url": "/ppfurtado/ranekapp/index.html"
+  },
+  {
+    "revision": "1df874db7bcfa8392766",
+    "url": "/ppfurtado/ranekapp/static/css/main.70d3ebe4.chunk.css"
+  },
+  {
+    "revision": "584ea1e44c5e89ab9330",
+    "url": "/ppfurtado/ranekapp/static/js/2.4baa5752.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ppfurtado/ranekapp/static/js/2.4baa5752.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1df874db7bcfa8392766",
+    "url": "/ppfurtado/ranekapp/static/js/main.cd731b06.chunk.js"
+  },
+  {
+    "revision": "1f23fc7dcbe1a97707d7",
+    "url": "/ppfurtado/ranekapp/static/js/runtime-main.ec37def7.js"
+  },
+  {
+    "revision": "20d1d7bb569e9158b47473eba9c4ca2c",
+    "url": "/ppfurtado/ranekapp/static/media/contato.20d1d7bb.jpg"
+  }
+]);
