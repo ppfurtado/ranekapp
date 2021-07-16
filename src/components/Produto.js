158a0465bfeb87ed8df4e60a676b6a771c277e89
ppfurtado <furtado.pedropaulo@gmail.com>
2021-07-16 13:44:24 -0300

    test

--- src/components/Produto.js
+++ src/components/Produto.js
@@ -1,54 +1,50 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import Head from './Head'
-import styles from './Produto.module.css'
+import React from "react";
+import { useParams } from "react-router-dom";
+import Head from "./Head";
+import styles from "./Produto.module.css";
 
 const Produto = () => {
+  const [produto, setproduto] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
+  const { id } = useParams();
 
-  const [produto, setproduto] = React.useState(null)
-  const [loading, setLoading] = React.useState(false)
-  const [error, setError] = React.useState(null)
-  const { id } = useParams()
-
-  React.useEffect(()=> {
-    async function fetchProduto(url){
+  React.useEffect(() => {
+    async function fetchProduto(url) {
       try {
-        setLoading(true)
-        const response = await fetch(url)
-        const json = await response.json()
-        setproduto(json)
+        setLoading(true);
+        const response = await fetch(url);
+        const json = await response.json();
+        setproduto(json);
       } catch (erro) {
-        setError('Um erro ocorreu')
+        setError("Um erro ocorreu");
       } finally {
-        setLoading(false)
+        setLoading(false);
       }
     }
-    fetchProduto(`https://ranekapi.origamid.dev/json/api/produto/${id}`)
-  },[id])
+    fetchProduto(`https://ranekapi.origamid.dev/json/api/produto/${id}`);
+  }, [id]);
 
-
-  if(loading) return <div className="loading"></div>
-  if(error) return <p>{error}</p>
-  if(produto === null) return null
+  if (loading) return <div className="loading"></div>;
+  if (error) return <p>{error}</p>;
+  if (produto === null) return null;
   return (
     <section className={styles.Container + " animeLeft"}>
       <Head title={`Ranek | ${produto.nome}`} descricao={produto.descricao} />
       <div className={styles.Images}>
-        {produto.fotos.map(foto => (
+        {produto.fotos.map((foto) => (
           <div key={foto.src}>
-            <img className={styles.Img} src={foto.src} alt={foto.titulo}/>
+            <img className={styles.Img} src={foto.src} alt={foto.titulo} />
           </div>
         ))}
       </div>
       <div className={styles.Informacoes}>
-        <h1> {produto.nome} </h1>  
-        <h1 className={styles.preco}> R$ { produto.preco } </h1>
-        <p>
-          Descrição: {produto.descricao}
-        </p> 
+        <h1> {produto.nome} </h1>
+        <h1 className={styles.preco}> R$$ {produto.preco} </h1>
+        <p>Descrição: {produto.descricao}</p>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Produto
+export default Produto;
--- src/components/Produtos.js
+++ src/components/Produtos.js
@@ -1,39 +1,36 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Head from './Head'
-import styles from './Produtos.module.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import Head from "./Head";
+import styles from "./Produtos.module.css";
 
 const Produtos = () => {
+  const [produtos, setProdutos] = React.useState(null);
 
-  const [produtos, setProdutos] = React.useState(null)
- 
-  React.useEffect( ()=> {
-    async function fetchData(){
-      const data = await fetch('https://ranekapi.origamid.dev/json/api/produto')
-      const json = await data.json()
-      setProdutos(json)
-      
+  React.useEffect(() => {
+    async function fetchData() {
+      const data = await fetch(
+        "https://ranekapi.origamid.dev/json/api/produto"
+      );
+      const json = await data.json();
+      setProdutos(json);
     }
-    fetchData()
-    
-  },[])
+    fetchData();
+  }, []);
 
-  if(produtos === null) return null
+  if (produtos === null) return null;
   return (
     <>
-      <Head title="Ranek" description="Página onde encontramos os produtos" />  
-      <section className={styles.produtos + ' animeLeft'}>
-          {
-            produtos.map((produto) =>(
-              <Link to={`produto/${produto.id}`} key={produto.id} >
-                  <h1>{produto.nome}</h1>
-                  <img src={produto.fotos[0].src} alt={produto.fotos.titulo} />
-              </Link>
-            ))
-          }
+      <Head title="Ranek" description="Página onde encontramos os produtos" />
+      <section className={styles.produtos + " animeLeft"}>
+        {produtos.map((produto) => (
+          <Link to={`produto/${produto.id}`} key={produto.id}>
+            <h1>{produto.nome}</h1>
+            <img src={produto.fotos[0].src} alt={produto.fotos.titulo} />
+          </Link>
+        ))}
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Produtos
+export default Produtos;
