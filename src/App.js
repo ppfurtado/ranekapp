d9e9ef3c6651cb7afc175bb92d0ad63283c8d30e
ppfurtado <furtado.pedropaulo@gmail.com>
2021-07-16 13:49:31 -0300

    swap BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 
 import "./App.css";
 
@@ -12,7 +12,7 @@ import Produtos from "./components/Produtos";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Header />
         <div className="content">
           <Routes>
@@ -23,7 +23,7 @@ function App() {
           </Routes>
         </div>
         <Footer />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
