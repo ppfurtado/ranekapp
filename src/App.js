1300190854243e117d280fb0b4ea31d8647e02b5
ppfurtado <furtado.pedropaulo@gmail.com>
2021-07-16 13:04:21 -0300

    fix path home

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import './App.css'
+import "./App.css";
 
-import Contato from './components/Contato';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Produto from './components/Produto';
-import Produtos from './components/Produtos';
+import Contato from "./components/Contato";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Produto from "./components/Produto";
+import Produtos from "./components/Produtos";
 
 function App() {
-
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
         <div className="content">
           <Routes>
-            <Route path="/" element={ <Produtos />}  />
-            <Route path="/produto/:id" element={ <Produto /> } />
-            <Route path="/contato" element={ <Contato /> } />
+            <Route path="/ranekapp/" element={<Produtos />} />
+            <Route path="/produto/:id" element={<Produto />} />
+            <Route path="/contato" element={<Contato />} />
           </Routes>
         </div>
         <Footer />
